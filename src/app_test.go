cbd74c7298794d81b6faae20edc84919f01d0dff
(Joca) <jocajunior77@gmail.com>
2020-03-23 17:13:08 -0300

    testando erro

--- src/app_test.go
+++ src/app_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestSum(t *testing.T ) {
-    result := sum(5, 5)
+    result := sum(6, 5)
 
     if result != 10 {
         t.Errorf("Soma de 5 + 5 != %d", result)
